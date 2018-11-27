bd8e585cf198b522113b0067089b06198220df48
Patrick Krukenfellner <krukenfellner.patrick@student.htlwrn.ac.at>
2018-11-27 07:33:01 +0100

    Implement interpret function

--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"errors"
-	_ "os"
+	"os"
 	"fmt"
+	"strings"
+	"strconv"
 )
 
 type Queen struct {
-	x_pos int
-	y_pos int
+	x_pos int16
+	y_pos int16
 	color string
 }
 
@@ -47,7 +49,7 @@ func checkAttack(q1, q2 *Queen) bool {
 	return false
 }
 
-func getQueen(x_pos, y_pos int, color string) (*Queen, error) {
+func getQueen(x_pos, y_pos int16, color string) (*Queen, error) {
 	if (x_pos >= 0 && x_pos < 7) && (y_pos >= 0 && y_pos < 7) {
 		return &Queen{x_pos, y_pos, color}, nil
 	} else {
@@ -57,16 +59,26 @@ func getQueen(x_pos, y_pos int, color string) (*Queen, error) {
 
 func interpret(args []string) (*Queen, *Queen, error) {
 	//TODO -> Interpret Commandline Parameter and return the interpretation
-	return nil, nil, nil
+	q1_val := strings.Split(args[1], ",")
+	q2_val := strings.Split(args[2], ",")
+
+	q1_x, conv_err := strconv.Atoi(q1_val[0])
+	q1_y, conv_err := strconv.Atoi(q1_val[1])
+	q2_x, conv_err := strconv.Atoi(q2_val[0])
+	q2_y, conv_err := strconv.Atoi(q2_val[1])
+
+	if conv_err != nil {
+		return nil, nil, conv_err
+	}
+
+	q1, err := getQueen(int16(q1_x), int16(q1_y), q1_val[2])
+	q2, err := getQueen(int16(q2_x), int16(q2_y), q2_val[2])
+
+	return q1, q2, err
 }
 
 func main() {
-	q1, err := getQueen(1, 2, "black")
-	q2, err := getQueen(0, 1, "white")
-
-	if err != nil{
-		panic(err)
-	}
+	q1, q2, _ := interpret(os.Args)
 
 	fmt.Println(checkAttack(q1, q2))
 }
